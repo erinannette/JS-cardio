0632a7d1297855df1051fa467bdf8ed8915252e4
Erin <erin_maas@yahoo.com>
2018-12-02 00:41:41 -0500

    almost there with makeDeliciousSoup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,7 +6,7 @@ const scales = (a, b, c, d, e, f, g, h) => {
 }
 scales("do", "re", "mi", "fa", "so", "la", "ti", "do");
 
-// 2. Write a single function makeDeliciousSoup that takes  two arguments: a callback function called cook() and an array of RAW ingredients.
+// 2. Write a single function makeDeliciousSoup that takes two arguments: a callback function called cook() and an array of RAW ingredients.
 // The cook function returns the string 'The method of cooking is' and then the argument passed into the cook function.
 
 // The result of the makeDeliciousSoup function should appear in the console like this:
@@ -21,16 +21,28 @@ scales("do", "re", "mi", "fa", "so", "la", "ti", "do");
 // it looks to me like the function does two things:
 // 1. it defines the method of cooking (boiling)
 // 2. it adds in the word "cooked" to each ingredient in the array.
+//    So I need to go through each item in the array and add in the word "cooked"
 
-// 1. 
+const cook = (method) => {
+    return `The method of cooking is ${method}.`
+}
+console.log(cook("boiling"));
 
-// const makeDeliciousSoup = (a,b){
-//     const ingredients = ["onions", "carrots", "celery"];
-//     const cook = () => {
+// this function is adding the word "cooked" to each item in the array
+const makeDeliciousSoup = (callback, ingredient) => {
+    const soup = [];
+    ingredient.forEach(ing => {
 
-//     }
-// }
+        // add the string "cooked" to each item in the array
+        let cookedIngredient = `cooked ${ingredient}`;
+        // then push each item into the cookedIngredients array
+        soup.push(cookedIngredient);
+        
+    });
+};
 
+makeDeliciousSoup(cook("boiling"), ["carrots", "celery", "noodles"]);
+console.log(makeDeliciousSoup);
 
 // 2.5
 // Write a function that returns the EXACT SAME THING but takes cooking ingredients not as an array but as strings passed into the makeDeliciousSoup function. 
@@ -59,15 +71,17 @@ scales("do", "re", "mi", "fa", "so", "la", "ti", "do");
 
 const canadianMagazines = ["24 images", "Alternatives Journal", "abOUT", "Above&Beyond", "L'Action nationale", "L'actualité", "Adbusters", "Advisor's Edge", "Alberta Report", "Algoma Ink", "Alive Magazine", "Auto Atlantic", "BC Report", "The Body Politic", "Brave Words & Bloody Knuckles", "Briarpatch", "Edmonton Gazette", "Brick", "Broken Pencil", "Canada World View", "Canada's History", "Canadian Art", "Canadian Business", "On Spec", "Canadian Dimension", "Canadian Forum", "Canadian Geographic", "Canadian Home & Country", "Canadian Literature", "Canadian Living", "Canadian Parliamentary Review", "ChartAttack", "Chatelaine", "Checkerspot", "Chickadee", "Ciné-Bulles", "Cinema Canada", "Cité Libre", "Contemporary Verse 2", "Corporate Knights", "Country Music News", "The Dalhousie Review", "Cult MTL", "Dogs in Canada", "The Dorchester Review", "Downhome", "Drug Facts for Young People", "Elementary Safety Book For Children", "Esprit de Corps", "Exclaim!", "Every Day Fiction", "fps", "fab", "fab", "abOUT", "Fashion", "Faze", "The Fiddlehead", "Flare", "Flare", "Flare", "Flare", "Frank", "Fugues", "Fuse", "GayCalgary", "Geez", "Geist", "Géographica", "Glow", "Golden Words", "Graffiti", "Harrowsmith", "HighGrader", "The Hockey News", "Homemakers", "Hunter and Cook", "International Hair Route Magazine", "Inuktitut", "KidsWorld", "Justice Magazine", "Legion Magazine", "Literary Review of Canada", "LOU LOU", "Maandblad de Krant", "Maclean's", "The Magazine", "Maisonneuve", "Maisonneuve", "The Malahat Review", "Mehfil Magazine", "Memewar", "MoneySense", "MONTECRISTO", "Les Mouches Fantastiques", "Music Express", "Flare", "Muskrat Magazine", "Natural Life", "Neo-opsis Science Fiction Magazine", "The New Quarterly", "Newfoundland Quarterly", "The Nerve", "Nightlife", "Northern Ontario Business", "On Spec", "Ontario Out of Doors", "Opera Canada", "Ottawa Business Journal", "Outdoor Canada", "Outlooks", "Owl", "Paaras", "Peace Magazine", "Photosho", "Playback", "Plenitude", "Popjournalism", "Pound", "Prairie Fire", "Quill & Quire", "Realm", "Rites", "Rockstar Weekly", "Room", "RPM", "Rue Morgue", "Ryerson Review of Journalism", "Saskatchewan History", "Saturday Night", "SAY", "Scoregolf", "Séquences", "Shameless", "Sharp", "Shift", "Sir John Magazine", "Siren", "Spacing", "Sportsnet Magazine", "Stitches: The Journal of Medical Humour", "Straight Goods", "Style at Home", "Swerve", "Take One", "Tamarack Review", "This Magazine", "TISH", "Toro", "Toronto Life", "Tribe", "TV Guide", "Up Here", "Uppercase", "Urban Male Magazine", "University Magazine", "Urban North - A Lifestyle Magazine", "Vancouver Magazine", "Vice 160", "The Walrus", "Wasted Youth", "Wayves", "Wayves", "Weddingbells", "The Western Producer", "Western Standard", "What If?", "Xtra!", "Xtra Ottawa", "Xtra Vancouver"]
 
+// input: array of magazine titles
+// output: a new array containing only "O" titles
+
+// 1. Iterate through the array
+// 2. If a title starts with O, put that into a new array
+
 // const magazinesO = function () {
 //     if (let i = 0; i < canadianMagazines.length; i++) {
 
 //     }
 // }
-
-// 1. Iterate through the array
-// 2. If a 
-
 
 
 
